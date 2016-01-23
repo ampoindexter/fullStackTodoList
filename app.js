30c7f5e298978201a191f1d92317fbd6dc81aa9b
Alicia Poindexter <alcampbel@gmail.com>
2016-01-22 17:39:39 -0800

    add modularization

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const PORT = process.env.PORT || 3000;
-var DATAFILE = './todos.json';
 
 var express = require('express');
 var logger = require('morgan');
@@ -15,60 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.use('/todos', require('./routes/todos'));
+
 app.get('/', function(req, res) {
   var html = fs.readFileSync('./index.html').toString();
   res.send(html);
 });
-
-app.get('/todos', function(req, res) {
-  retrieveTodos(function(todos) {
-    res.send(todos);
-  });
-});
-
-app.post('/todos', function(req, res) {
-  var newTodo = req.body;
-  newTodo.isComplete = false;
-  retrieveTodos(function(todos) {
-    todos.push(newTodo);
-    writeTodos(todos, function(err) {
-      res.send(todos);
-    });
-  });
-});
-
-app.delete('/todos/:index', function(req, res) {
-  var index = req.params.index;
-  retrieveTodos(function(todos) {
-    todos.splice(index, 1);
-    writeTodos(todos, function(err) {
-      res.send(todos);
-    });
-  });
-});
-
-app.put('/todos/:index', function(req, res) {
-  var index = req.params.index;
-  retrieveTodos(function(todos) {
-    todos[index].isComplete = !todos[index].isComplete;
-    writeTodos(todos, function(err) {
-      res.send(todos);
-    });
-  });
-});
-
-function writeTodos(todos, cb) {
-  fs.writeFile(DATAFILE, JSON.stringify(todos), function(err) {
-    cb(err);
-  });
-}
-
-function retrieveTodos(cb) {
-  fs.readFile(DATAFILE, function(err, data) {
-    var todos = JSON.parse(data);
-    cb(todos);
-  });
-}
 
 app.listen(PORT, function(){
   console.log('Listening on port ', PORT);
